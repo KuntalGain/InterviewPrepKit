d449018a83612e35ee37c21f461f1ca08ff86af7
Kuntal Gain <101191826+KuntalGain@users.noreply.github.com>
2022-10-23 10:57:03 +0530

    Solved!!!

---
+++ Easy/3-Longest_common_prefix.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& s) {
+        
+        int ans = s[0].length();      //length of the string
+        int n = s.size();             //length of the string array
+        
+      //traverse the every string elemnet
+        for(int i = 1 ; i < n ; i++){
+            int j = 0;
+            
+            while(j < s[i].length() && s[i][j] == s[0][j])
+                j++;            //update j
+            
+            ans = min (ans , j);
+        }
+        
+        return s[0].substr(0,ans);
+    }
+};
